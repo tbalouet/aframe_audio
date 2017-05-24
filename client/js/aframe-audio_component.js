320822e0951c66d03b775d34c84f34b0b6ed9a93
Thomas Balouet <t.balouet@gmail.com>
2017-05-24 19:23:22 +0200

    first event handler to play sound on mousedown

--- client/js/aframe-audio_component.js
+++ client/js/aframe-audio_component.js
@@ -5,6 +5,14 @@
 (function(){
   "use strict";
 
+  const soundBaseURL   = "https://surikov.github.io/webaudiofontdata/sound/";
+
+  //Instanciate Global Audio Functionnalities
+  let AudioContextFunc          = window.AudioContext || window.webkitAudioContext;
+  window.audioComp              = window.audioComp || {};
+  window.audioComp.audioContext = new AudioContextFunc();
+  window.audioComp.player       = new WebAudioFontPlayer();
+
   AFRAME.registerComponent('aframe-audio', {
     schema: {
       soundName: {type: 'string', default: '_drum_35_17_JCLive_sf2_file'},
@@ -19,13 +27,26 @@
      * @return {[type]} [description]
      */
     init: function () {
+      var that = this;
+
+      let scriptFile = document.createElement('script');
+      scriptFile.type = 'text/javascript';
+      scriptFile.async = true;
+      scriptFile.onload = function () {
+        window.audioComp.player.loader.decodeAfterLoading(window.audioComp.audioContext, that.data.soundName);
+      }
+      scriptFile.src = soundBaseURL + this.data.soundFile;
+      document.head.appendChild( scriptFile );
+
       /**
        * Function which plays souns when interacting with the entity
        * @return {[type]} [description]
        */
       this.onInteract = function(){
-        console.log("[AFRAME-Audio Component] Houston we've been clicked!");
-        player.queueWaveTable(audioContext, audioContext.destination, this.data.soundName, 0, 35, 3);
+        //Check if file has been instanciated
+        if(window[this.data.soundName]){
+          window.audioComp.player.queueWaveTable(window.audioComp.audioContext, window.audioComp.audioContext.destination, window[this.data.soundName], 0, 35, 3);
+        }
       }
 
       console.log("[AFRAME-Audio Component] Component initialized", this.data.soundName);
@@ -37,7 +58,7 @@
      * @return {[type]} [description]
      */
     update: function () {
-      this.el.addEventListener('click', this.onInteract);
+      this.el.addEventListener('mousedown', this.onInteract.bind(this));
     },
     /**
      * Handles component, called on each animation frame
@@ -47,25 +68,6 @@
      */
     tick: function (time, timeDelta) {
     },
-    /**
-     * Called when entity pauses.
-     * Use to stop or remove any dynamic or background behavior such as events.
-     * @return {[type]} [description]
-     */
-    pause: function () { },
-
-    /**
-     * Called when entity resumes.
-     * Use to continue or add any dynamic or background behavior such as events.
-     * @return {[type]} [description]
-     */
-    play: function () { },
-
-    /**
-     * Handles component removal, called when a component is removed (e.g., via removeAttribute).
-     * Generally undoes all modifications to the entity.
-     * @return {[type]} [description]
-     */
     remove: function () {
     }
   });
